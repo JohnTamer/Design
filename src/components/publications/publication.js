942a68ff7144e9f103f438aa54e40a2f94d9b209
JohnTamerZaki <johntamer98@gmail.com>
2022-07-01 14:00:36 +0200

    finishing the publication

--- src/components/publications/publication.js
+++ src/components/publications/publication.js
@@ -12,6 +12,38 @@ export const Publication = () => {
             src={require("../../assets/images/pub.png")}
             alt=""
           />
+          <div className="publication__paragraph">
+            <div className="publication__paragraph__one">
+              Consulting Project
+            </div>
+            <div className="publication__paragraph__two">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry.
+            </div>
+            <button className="publication__btn">DOWNLOAD</button>
+          </div>
+        </div>
+
+        <div className="publication__div">
+          <img
+            className="publication__img"
+            src={require("../../assets/images/pub.png")}
+            alt=""
+          />
+          <div className="publication__paragraph">
+            <div className="publication__paragraph__one">
+              Consulting Project
+            </div>
+            <div className="publication__paragraph__two">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry.
+            </div>
+            <button className="publication__btn">DOWNLOAD</button>
+          </div>
         </div>
         <div className="publication__div">
           <img
@@ -19,13 +51,18 @@ export const Publication = () => {
             src={require("../../assets/images/pub.png")}
             alt=""
           />
-        </div>
-        <div className="publication__div">
-          <img
-            className="publication__img"
-            src={require("../../assets/images/pub.png")}
-            alt=""
-          />
+          <div className="publication__paragraph">
+            <div className="publication__paragraph__one">
+              Consulting Project
+            </div>
+            <div className="publication__paragraph__two">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry.
+            </div>
+            <button className="publication__btn">DOWNLOAD</button>
+          </div>
         </div>
       </div>
     </div>

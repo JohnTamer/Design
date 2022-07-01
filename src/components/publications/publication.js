e0520cdf3468291250382f7439ccdf364f47bcb2
JohnTamerZaki <johntamer98@gmail.com>
2022-07-01 13:42:31 +0200

    some updates on publications

--- src/components/publications/publication.js
+++ src/components/publications/publication.js
@@ -1,4 +1,33 @@
 import "./publication.css";
 export const Publication = () => {
-  return <div className="publication"></div>;
+  return (
+    <div className="publication">
+      <div className="publication__one">
+        OUR <span className="publication__text">PUBLICATIONS</span>
+      </div>
+      <div className="publication__container">
+        <div className="publication__div">
+          <img
+            className="publication__img"
+            src={require("../../assets/images/pub.png")}
+            alt=""
+          />
+        </div>
+        <div className="publication__div">
+          <img
+            className="publication__img"
+            src={require("../../assets/images/pub.png")}
+            alt=""
+          />
+        </div>
+        <div className="publication__div">
+          <img
+            className="publication__img"
+            src={require("../../assets/images/pub.png")}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };

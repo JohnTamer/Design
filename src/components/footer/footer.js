996853a2cf3adfe55f3da1120f53f42437e0723e
JohnTamerZaki <johntamer98@gmail.com>
2022-07-03 15:40:00 +0200

    finished the footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -22,18 +22,43 @@ export const Footer = () => {
           Signup for industry alerts, News & Insights
         </div>
         <div>
-          {/* <img
-            className="footer__round"
-            src={require("../../assets/images/footerround.png")}
-            alt=""
-          />
-          <img
-            className="footer__rec"
-            src={require("../../assets/images/footerrec.png")}
-            alt=""
-          /> */}
           <input className="footer__input" placeholder="your email" />
           <button className="footer__btn">Submit</button>
+        </div>
+      </div>
+      <div className="footer__mid">
+        <div>ABOUT CROWN</div>
+        <div>who we are</div>
+        <div>What we do</div>
+        <div>Our mission</div>
+        <div>Our vision</div>
+      </div>
+      <div className="footer__aftermid">
+        <div>QUICK LINKS</div>
+        <div>Our Services</div>
+        <div>All Projects</div>
+        <div>Our Partners</div>
+        <div>News</div>
+      </div>
+      <div className="footer__last">
+        <div> CONTACT US</div>
+        <div>
+          <i class="fa fa-phone"></i>
+          0123456789
+        </div>
+        <div>
+          <i class="fa fa-envelope" aria-hidden="true"></i>info@crown.com
+        </div>
+        <div>
+          <i class="fa fa-map-marker" aria-hidden="true"></i>123,Street Name -
+          Cairo
+        </div>
+        <div className="footer__follow">FOLLOW US</div>
+        <div className="footer__icons">
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-linkedin"></i>
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-google-plus-g"></i>
         </div>
       </div>
     </div>
